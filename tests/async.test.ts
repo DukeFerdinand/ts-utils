a2bd1902e3b2bd66bba19492b9e54142fdbd0fe4
Doug Hooker <DouglassHooker@gmail.com>
2020-11-28 13:19:57 -0600

    Add tests for async wrapper

--- tests/async.test.ts
+++ tests/async.test.ts
@@ -1,5 +1,20 @@
-describe('Async test suite', () => {
-  test('placeholder test', () => {
-    expect(true).toBe(true)
+import { Err, Ok } from '@dukeferdinand/ts-results'
+import { Async } from '../lib/async'
+
+const { asyncWrapped } = Async
+
+describe('Async wrapped util', () => {
+  it('returns Ok wrapped values on successful calls', async () => {
+    const res = await asyncWrapped(() => 5)()
+    expect(res).toBeInstanceOf(Ok)
+    expect(res.unwrap()).toBe(5)
+  })
+
+  it('catches and returns error objects', async () => {
+    const res = await asyncWrapped(() => {
+      throw new Error('[ Async Wrapped Error ] Test error for asyncWrapped')
+    })()
+    expect(res).toBeInstanceOf(Err)
+    expect(res.unwrapErr()).toMatchSnapshot()
   })
 })
