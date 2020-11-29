bac4dbf62f9db03caeb58baa02222c41b28f1d32
Doug Hooker <DouglassHooker@gmail.com>
2020-11-28 18:50:39 -0600

    Move util package types to namespace

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
-export declare type Nullable<T> = T | null;
-export declare type Optional<T> = T | undefined;
+export namespace UtilTypes {
+  export declare type Nullable<T> = T | null;
+  export declare type Optional<T> = T | undefined;
+}
 
 export { Sync } from './sync'
 export { Async } from './async'
