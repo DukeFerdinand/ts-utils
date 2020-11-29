cfe03dfb8bc8a2e09cb364f43c9c11a022684b19
Doug Hooker <DouglassHooker@gmail.com>
2020-11-28 20:03:35 -0600

    Add smartFetch and smartFetch tests! :D

---
+++ lib/fetch/__tests__/smartfetch.test.ts
@@ -0,0 +1,83 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// I'm using a jsdom environment here as I'm explicitly testing browser functionality vs node stuff for now
+import { Err, Ok } from '@dukeferdinand/ts-results'
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
+
+import { SmartFetch } from '..'
+
+describe('SmartFetch config utils', () => {
+  beforeEach(() => {
+    SmartFetch.initSmartFetch({
+      baseUrl: 'https://google.com'
+    })
+  })
+  describe('when initializing', () => {
+    it('sets __SMART_FETCH_CONFIG__', () => {
+      expect(window.__SMART_FETCH_CONFIG__?.baseUrl).toBe('https://google.com')
+    })
+    it('overwrites previous config if called again', () => {
+      SmartFetch.initSmartFetch({
+        baseUrl: 'https://twitch.tv'
+      })
+      expect(window.__SMART_FETCH_CONFIG__?.baseUrl).toBe('https://twitch.tv')
+    })
+  })
+
+  describe('when getting', () => {
+    it('correctly retrieves __SMART_FETCH_CONFIG__', () => {
+      const conf = SmartFetch.getSmartFetchConfig()
+      expect(conf?.baseUrl).toBe('https://google.com')
+    })
+  })
+})
+
+describe('SmartFetch http client wrapper', () => {
+  it('returns good data wrapped in Ok variant', async () => {
+    fetchMock.mockResponse(JSON.stringify({ "ip": "70.113.52.10" }))
+
+    const res = await SmartFetch.smartFetch(SmartFetch.RequestMethods.GET, '/fake-ip-route?format=json')
+    expect(res).toBeInstanceOf(Ok)
+    expect(res.unwrap()).toEqual({ "ip": "70.113.52.10" })
+  })
+
+  it('returns "bad" http statuses as Err variants', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ error: 'Message here' }), { status: 500, headers: { 'content-type': 'application/json' } })
+    const res = await SmartFetch.smartFetch(SmartFetch.RequestMethods.GET, '/bad-route')
+
+    expect(res).toBeInstanceOf(Err)
+    expect(res.unwrapErr()).toEqual({ "error": "Message here" })
+  })
+
+  it('uses shouldThrow to catch any non standard errors', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ error: 'Message here' }), { status: 200, headers: { 'content-type': 'application/json' } })
+
+    // Mock example to catch any response with an 'error' key, but only if 'error' is not an empty string
+    function shouldThrow(res: { [index: string]: any }) {
+      if (Object.keys(res).includes('error') && res.error !== '') {
+        return true
+      }
+      return false
+    }
+
+    // Break because error is not an empty string
+    const shouldError = await SmartFetch.smartFetch(SmartFetch.RequestMethods.GET, '/bad-route', null, {
+      shouldThrow
+    })
+
+    expect(shouldError).toBeInstanceOf(Err)
+
+    // Specifically allow error key with empty string in this test
+    fetchMock.mockResponseOnce(JSON.stringify({ error: '' }), { status: 200, headers: { 'content-type': 'application/json' } })
+
+    const shouldNOTError = await SmartFetch.smartFetch(SmartFetch.RequestMethods.GET, '/okay-route', null, {
+      shouldThrow
+    })
+
+    expect(shouldNOTError).toBeInstanceOf(Ok)
+
+  })
+})
---
+++ lib/fetch/index.ts
@@ -0,0 +1,77 @@
+import { err, ok, Result } from "@dukeferdinand/ts-results";
+
+import { Sync } from '../sync'
+
+const { toString } = Sync
+
+export namespace SmartFetch {
+  export interface GlobalConfig extends Omit<RequestInit, "body" | "method"> {
+    baseUrl?: string;
+    shouldThrow?: (response: any) => boolean
+  }
+
+  export interface LocalConfig extends RequestInit {
+  }
+
+  export enum RequestMethods {
+    GET = 'GET',
+    POST = 'POST',
+    DELETE = 'DELETE',
+    PUT = 'PUT',
+    PATCH = 'PATCH'
+  }
+
+  export function initSmartFetch(config: GlobalConfig) {
+    window.__SMART_FETCH_CONFIG__ = config
+  }
+
+  export function getSmartFetchConfig() {
+    return window.__SMART_FETCH_CONFIG__ || {} as GlobalConfig
+  }
+
+
+  export async function smartFetch<T, E extends Error>(
+    method: RequestMethods, url: string, body?: any, config: GlobalConfig = {}
+  ): Promise<Result<T, E>> {
+    const { shouldThrow, ...local } = config
+    if (!window) {
+      throw new Error('[ Smart Fetch ] Warning: cannot find window object. Isomorphic and other server side implementations are not available yet.')
+    }
+
+    let localConfig: GlobalConfig & RequestInit = { ...local, method }
+    if (window && window.__SMART_FETCH_CONFIG__) {
+      // local will always override global unless otherwise allowed in the future
+      localConfig = { ...window.__SMART_FETCH_CONFIG__, ...localConfig }
+    }
+
+    if (body) {
+      localConfig = { ...localConfig, body: toString(body) }
+    }
+
+    try {
+      // Actual fetch call
+      const res = await fetch(url, localConfig)
+
+      // Res isn't a 400, 500, or other standard error
+      if (res.ok) {
+        const parsed: T | E = await res.json()
+
+        // Custom error parser in case you need 200 but have an `error` key or similar need
+        if (shouldThrow && shouldThrow(parsed)) {
+          return err<E>(parsed as E)
+        }
+
+        // Everything is okay, return okay data
+        return ok(parsed as T)
+      } else {
+
+        // Standard HTTP error or similar bad request occurred
+        const parsed: T | E = await res.json()
+        return err<E>(parsed as E)
+      }
+    } catch (e) {
+      // Something broke that was not able to be caught by normal means
+      return err<E>(e)
+    }
+  }
+}
